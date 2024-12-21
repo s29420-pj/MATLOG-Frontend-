54b6e0617512220267f723abfc18cfe5692d3601
s27553-pj <s27553@pjwstk.edu.pl>
2024-12-21 16:23:36 +0100

    add logo and update route

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: "./login.component.html",
   styleUrl: './login.component.css'
