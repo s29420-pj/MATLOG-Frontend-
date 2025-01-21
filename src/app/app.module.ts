bbeabb199598f8f97febd7f6aee5549f80fc8631
s27553-pj <s27553@pjwstk.edu.pl>
2025-01-21 20:33:32 +0100

    update responsive layout and add profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 import {HttpClientModule} from '@angular/common/http';
+import { NazwaKomponentuComponent } from './nazwa-komponentu/nazwa-komponentu.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     // Główny komponent aplikacji
+  
+    NazwaKomponentuComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
